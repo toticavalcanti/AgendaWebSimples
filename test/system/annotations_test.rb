d7e22d06698ce66a3acedfad388862f8c53ce37f
toti.cavalcanti <toticavalcanti@hotmail.com>
2017-05-25 16:46:22 -0300

    upload remember

---
+++ test/system/annotations_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class AnnotationsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit annotations_url
+  #
+  #   assert_selector "h1", text: "Annotation"
+  # end
+end
